9ce112e1f4ec1edf158c279d94ecc86a93f0bfd8
DavidC001 <davidecavicchini@hotmail.com>
2024-06-21 18:29:10 +0200

    fix: datasets preproocessing

--- dataloaders/pre-process.py
+++ dataloaders/pre-process.py
@@ -1,7 +1,12 @@
 from dataloaders.preprocess_nombank import main as preprocess_UP
 from dataloaders.preprocess_UP import main as preprocess_nombank
+import os
 
 def preprocess_datasets(UP=True, nombank=True):
+    # create folder datasets/preprocessed
+    if (not os.path.exists('datasets/preprocessed')):
+        os.makedirs('datasets/preprocessed')
+    
     if UP:
         preprocess_UP()
     if nombank:
