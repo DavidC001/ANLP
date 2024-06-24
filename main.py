526b98d1f2dc88784a54524d2aa8443937ce54ea
DavidC001 <davidecavicchini@hotmail.com>
2024-06-24 12:58:05 +0200

    feat: improved main with tests definition

--- main.py
+++ main.py
@@ -9,13 +9,58 @@ import torch
 def train_SRL():
     train_loader, val_loader, test_loader, num_senses, num_roles = get_dataloaders("datasets/preprocessed/", batch_size=64, shuffle=True)
 
-    model = SRL_BERT("bert-base-uncased", num_senses, num_roles, [50], device='cuda', combine_method='gating', norm_layer=True)
-    train(model, train_loader, val_loader, test_loader,
-          epochs=10, init_lr=0.0001, scheduler_step=2, scheduler_gamma=0.9,
-          device='cuda', name='SRL_BERT_TEST')
+    tests = {
+        "SRL_BERT_concat_100": {
+            "combine_method": "concat",
+            "layers": [100],
+            "norm_layer": False
+        },
+        "SRL_BERT_concat_100_norm": {
+            "combine_method": "concat",
+            "layers": [100],
+            "norm_layer": True
+        },
+        "SRL_BERT_mean_100": {
+            "combine_method": "mean",
+            "layers": [100],
+            "norm_layer": False
+        },
+        "SRL_BERT_mean_100_norm": {
+            "combine_method": "mean",
+            "layers": [100],
+            "norm_layer": True
+        },
+        "SRL_BERT_gated_100": {
+            "combine_method": "gated",
+            "layers": [100],
+            "norm_layer": False
+        },
+        "SRL_BERT_gated_100_norm": {
+            "combine_method": "gated",
+            "layers": [100],
+            "norm_layer": True
+        },
+        "SRL_BERT_gated_transform_100": {
+            "combine_method": "gating_transform",
+            "layers": [100],
+            "norm_layer": False
+        },
+        "SRL_BERT_gated_transform_100_norm": {
+            "combine_method": "gating_transform",
+            "layers": [100],
+            "norm_layer": True
+        },
+    }
 
-    # Save the model
-    torch.save(model.state_dict(), "models/SRL_BERT_TEST.pt")
+    for test in tests:
+        print(f"\nTraining model {test}")
+        model = SRL_BERT("bert-base-uncased", num_senses, num_roles, **tests[test])
+        train(model, train_loader, val_loader, test_loader,
+            epochs=10, init_lr=0.0001, scheduler_step=2, scheduler_gamma=0.9,
+            device='cuda', name=test)
+        # Save the model
+        torch.save(model.state_dict(), "models/SRL_BERT_TEST.pt")
+        print("-"*50)
 
 if __name__ == '__main__':
     train_SRL()
